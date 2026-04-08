f6b79336f687aa4a929e7b3569fc56aaecdf1cfb
TechnicalAkash0 <134239766+TechnicalAkash0@users.noreply.github.com>
2026-04-08 16:53:57 +0530

    Week1Contract.sol

--- Week1Contract.sol
+++ Week1Contract.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Week1Contract {
+
+    string public message;
+    address public owner;
+    uint public updateCount;
+
+    event MessageUpdated(string newMessage, address updatedBy);
+
+    constructor(string memory _message) {
+        message = _message;
+        owner = msg.sender;
+        updateCount = 0;
+    }
+
+    function setMessage(string memory _message) public {
+        message = _message;
+        updateCount += 1;
+
+        emit MessageUpdated(_message, msg.sender);
+    }
+
+    function getDetails() public view returns (string memory, address, uint) {
+        return (message, owner, updateCount);
+    }
+}
